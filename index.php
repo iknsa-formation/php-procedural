37b9020835284c05b9254544dae1fe2b3dfa1508
Moussa Camara <moussa.camara@iknsa.com>
2019-05-28 18:12:11 +0200

    step2 concatenation

--- index.php
+++ index.php
@@ -1,13 +1,32 @@
 <?php
-    $Prenom = 'Moussa';
-    $Nom = 'Camara';
-    $Age    = '28 ans';
-    $Profession = 'informaticien';
-  
-  	// Concanétation
-  	echo 'Bonjour ' . $Prenom. ' ' .$Nom . ', tu as ' . $Age . ' et ta profession est ' . $Profession . '.<br>';
-  
-  // Interpolation
-  echo 'Bonjour $Prenom <br>';
-  echo "Bonjour $Prenom";
+	echo 'Fichier: ' . __FILE__ . '<br>';
+	echo 'Ligne: ' . __LINE__ . '<br>';
+
+	class test {
+		function foo() {
+			echo 'Fonction: ' .    FUNCTION 	. "\n";
+			echo 'Classe: ' .    CLASS 	. "\n";
+		}
+	}
+	test::foo();
+	// ou alors
+	$test = new test();
+	$test->foo();
+
+
+/* LES CONSTANTES */
+
+	class MaClasse {
+		const CONSTANTE = "Texte ici";
+		function afficher()
+		{
+		echo self::CONSTANTE;
+		}
+	}
+
+	$instance = new MaClasse;
+	$instance->afficher();
+
+	define("CONSTANTE", "Texte ici --> define");  
+echo CONSTANTE; // affiche  "Texte ici"
 ?>
