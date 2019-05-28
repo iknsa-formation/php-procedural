35fcf5909b106b5c5878b75aa8e5ed3271f621a5
Moussa Camara <moussa.camara@iknsa.com>
2019-05-28 17:41:31 +0200

    initialisation variables

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    $bonjour = 'Bonjour';
+    $number = 3;
+    echo $bonjour.'<br>'.$number.'<br>';
+    $Prenom = 'Moussa';
+    $Age    = '28 ans';
+    $Profession = 'informaticien';
+    
+  	// Affichage
+   	echo 'Bonjour ' . $Prenom . ', tu as ' . $Age . ' et ta profession est ' . $Profession . '.<br>';
+  
+	$var0 = "Moussa";
+	$var1 = "Camara";
+	$var3 = 5.5 ;
+	$mois = array("octobre","novembre","décembre");
+	//$maVoiture = new $car;
+	//$maConnexion = mysql_connect("monServeur", "monNom","MonPass");
+	echo $var0.'<br>';
+	echo $var1.'<br>';
+	echo $var3.'<br>';
+	print($var0).'<br>';
+	echo "<br>";
+	print_r($mois);
+
+?>
