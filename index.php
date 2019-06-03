8ff9e3b77e67bacf20ac52787010bc1967511006
Moussa Camara <moussa.camara@iknsa.com>
2019-06-03 23:56:43 +0200

    method get and post

--- index.php
+++ index.php
@@ -1,10 +1,37 @@
+<?php
+    /* Utilisation de get */
+    /*if (isset($_GET['firstname']) &&  isset($_GET['lastname']) &&  isset($_GET['adresse']) &&  isset($_GET['fonction'])){
+        $prenom = $_GET['firstname'];
+        $nom = $_GET['lastname'];
+        $adresse = $_GET['adresse'];
+        $fonction = $_GET['fonction'];
+        echo "Prénom: " .$prenom . "<br>";
+        echo "Nom: " .$nom;
+        echo "Adresse: " .$adresse;
+        echo "Fonction: " .$fonction;
+    }*/
+    /* Fin utilisation de get */
+     /* Utilisation de post */
+    if (isset($_POST['firstname']) &&  isset($_POST['lastname']) &&  isset($_POST['adresse']) &&  isset($_POST['fonction'])){
+        $prenom = $_POST['firstname'];
+        $nom = $_POST['lastname'];
+        $adresse = $_POST['adresse'];
+        $fonction = $_POST['fonction'];
+        echo "Prénom: " .$prenom . "<br>";
+        echo "Nom: " .$nom;
+        echo "Adresse: " .$adresse;
+        echo "Fonction: " .$fonction;
+    }
+    /* Fin utilisation de POST */
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Formation Course - IKNSA</title>
+    <title>PHP Course - IKNSA</title>
     <link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 
   </head>
   <body>
@@ -28,20 +55,45 @@
         </div>
     </header>
 
-	<div class="jumbotron">
-	    <?php
-           //include 'index.html';
-           include 'include.php';
-           //require 'require.php';
-           // Chargement du fichier une et une seule fois
-           include_once 'include.php';
-           //include_once 'include.php';
-           /* Require */
-          require_once 'require.php';
-        ?>
-	</div>
+     <main>
+        <h1>Get in touch</h1>
+        <form method="post" action="index.php">
+            <div class="form-group zoneRadio">
+                <label>Title</label>
+                <input type="radio" name="title" tabindex="1"><span>Mr.</span>
+                <input type="radio" name="title" tabindex="2"><span>Mrs.</span>
+                <input type="radio" name="title" tabindex="3"><span>Miss.</span>
+            </div>
 
- 	<footer>
+            <div class="form-group">
+                <label for="firstname">FirstName*:</label>
+                <input type="text" name="firstname" id="firstname" tabindex="4">
+            </div>
+
+            <div class="form-group">
+                <label for="lastname">Lastname</label>
+                <input type="text" name="lastname" id="lastname">
+            </div>
+
+            <div class="form-group">
+                <label for="adresse">Adresse:</label>
+                <input type="text" name="adresse" id="adresse">
+            </div>
+                 <div class="form-group">
+                    <label for="fonction">Fonction:</label>
+                    <input type="text" name="fonction" id="fonction">
+                </div>
+            
+            <div class="required-fields">
+                *: Required fields
+            </div>
+            <div class="form-group">
+                <input type="submit" value="SEND">
+            </div>
+        </form>
+    </main>
+
+ <footer>
         copyright @iknsa.com
     </footer>
   </body>
